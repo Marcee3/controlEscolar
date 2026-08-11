a5e67184ac4325713e8e42229b15f520db9ad629
Marcela <marcelaguadalupe.palomar@gmail.com>
2026-08-10 22:10:51 -0600

    Configuracion de servidorcon express

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import session from 'express-session';
+import cors from 'cors';
+
+import db from './backend/config/database.js';
+import routes from './backend/routes/routes.js';
+
+import Carrera from './backend/models/Carrera.js';
+import Usuario from './backend/models/Usuario.js';
+import Materia from './backend/models/Materia.js';
+import Profesor from './backend/models/Profesor.js';
+import Alumno from './backend/models/Alumno.js';
+import Asignacion_Materia from './backend/models/Asignacion_Materia.js';
+import Calificacion from './backend/models/Calificacion.js';
+import Grupo from './backend/models/Grupo.js';
+import Periodo_Escolar from './backend/models/Periodo_Escolar.js';
+import "./backend/models/relaciones.js";
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Reemplaza con la URL de tu frontend
+    credentials: true // Permite enviar cookies con las solicitudes
+}));
+app.use(express.json());
+app.use(session({
+    secret: 'controlEscolar',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 } // 1 hora
+}));
+
+app.use('/api', routes);
+
+//Conexion a la base de datos
+db.authenticate()
+    .then(() => {
+        console.log('Conexión a la base de datos establecida correctamente.');
+    })
+    .catch((error) => {
+        console.error('Error al conectar a la base de datos:');
+        console.log(error);
+    });
+
+//Servidor
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor iniciado en el puerto http://localhost:${port}`);
+});
+
+export default app;
